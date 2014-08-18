7527c5dccb34fbfb0c8ea576f35f60b9a3c8a626
James Andariese <james.andariese@locationlabs.com>
2014-08-18 07:14:19 -0700

    Use sqlite3 to persist saved addresses

--- addresses.go
+++ addresses.go
@@ -1,8 +1,11 @@
 package notmuch_addresses
 
 import (
+	"errors"
 	"net/mail"
 	"os"
+	"code.google.com/p/go-sqlite/go1/sqlite3"
+	"log"
 )
 
 var (
@@ -16,8 +19,40 @@ var (
 	}
 )
 
-func GatherAddresses(filename string) (addresses []*mail.Address, err error) {
-	file, err := os.Open(filename)
+var (
+	conn *sqlite3.Conn
+)
+
+func InitDatabase(path string) error {
+	c, err := sqlite3.Open(path)
+	if err != nil {
+		return err
+	}
+	conn = c
+
+	conn.Exec(`CREATE TABLE IF NOT EXISTS address (
+                       address TEXT,
+                       count INTEGER
+                   );`)
+
+	return nil
+}
+
+func Close() (err error) {
+	err = conn.Close()
+	conn = nil
+	return
+}
+
+var ErrDatabaseNotOpen = errors.New("Database is not open")
+
+//func GatherAddresses(path string) (addresses []*mail.Address, err error) {
+func GatherAddresses(path string) (addresses int, err error) {
+	if conn == nil {
+		return 0, ErrDatabaseNotOpen
+	}
+
+	file, err := os.Open(path)
 	if err != nil {
 		return
 	}
@@ -28,8 +63,46 @@ func GatherAddresses(filename string) (addresses []*mail.Address, err error) {
 	for _,header := range headers {
 		tmp_addresses, err := msg.Header.AddressList(header)
 		if err == nil {
-			addresses = append(addresses, tmp_addresses...)
+			for _, address := range tmp_addresses {
+				//addresses = append(addresses, tmp_addresses...)
+				for {
+					var save bool
+					conn.Begin()
+					old_affected := conn.TotalRowsAffected()
+					err = conn.Exec(`UPDATE address
+                                                            SET count = count + 1
+                                                          WHERE address = ?`, 
+						address.String())
+					if err != nil {
+						// Can't save this email address.
+						// It wasn't because of a failed transaction.
+						log.Print("Couldn't increment or save address", address, err)
+						break
+					}
+					if conn.TotalRowsAffected() == old_affected {
+						err = conn.Exec(`INSERT INTO address (address, count) VALUES (?,1);`, address.String())
+						if err != nil {
+							log.Print("Couldn't save address", address, err)
+							break
+						}
+						save = true
+					} else {
+						save = false
+					}
+					if conn.Commit() == nil {
+						if save {
+							log.Print("Saved new address", address)
+						} else {
+							log.Print("Incremented address", address)
+						}
+						break
+					} else {
+						log.Print("Couldn't commit transaction.  Retrying.")
+					}
+				}
+			}
+			addresses += 1
 		}
 	}
-	return 
+	return
 }
--- notmuch_addresses/main.go
+++ notmuch_addresses/main.go
@@ -4,15 +4,23 @@ import (
 	"github.com/jamesandariese/notmuch_addresses"
 	//"net/mail"
 	"fmt"
+	"os"
 )
 
 func main() {
+	err := notmuch_addresses.InitDatabase(os.ExpandEnv("$HOME/.notmuch_addresses.sqlite3"))
+	if err != nil {
+		panic("Couldn't open database: " + err.Error())
+	}
+
 	addresses, err := notmuch_addresses.GatherAddresses("message")
 	if err != nil {
 		panic("Couldn't gather email addresses: " + err.Error())
 	}
-	for _, address := range addresses {
-		fmt.Printf("%#v\n", address.String())
+	fmt.Printf("Saved %d addresses\n", addresses)
+	err = notmuch_addresses.Close()
+	if err != nil {
+		panic("Couldn't close database: " + err.Error())
 	}
 }
 
